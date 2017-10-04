a21ba4de463464621303aaf45484cbf8cb47a911
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-04 13:57:02 -0600

    finish books_controller

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -2,16 +2,34 @@ let books = [];
 let id = 0;
 
 module.exports = {
-  create: (newBook) => {
-    
+  create: (req, res, next) => {
+    req.body.id = id;
+    id++;
+    var newBook = Object.assign({}, req.body);
+    books.push(newBook);
+    res.status(200).send(books);
   },
-  read: () => {
-    return books;
+  read: (req, res, next) => {
+    res.status(200).send(books);
   },
-  update: () => {
-
+  update: (req, res, next) => {
+    for (var i = 0; i < books.length; i++) {
+      if (books[i].id === +req.params.id) {
+        books[i] = Object.assign(books[i], req.body)
+      }
+    }
+    res.status(200).send(books);
+    /* -- using map
+    books.map((book) => {
+      if (req.params.id === book.id) {
+        book = Object.assign(book, req.body);
+      }
+    }
+    })
+    */
   },
-  delete: () => {
-
+  delete: (req, res, next) => {
+    books = books.filter((book) => book.id !== +req.params.id);
+    res.status(200).send(books);
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
+      app = express(),
       port = 3000,
-      app = express();
+      ctrl = require('./controllers/books_controller');
 
 // parses body into json, puts on req.body!!!
 app.use(bodyParser.json());
+
+
+app.get(`/api/getBooks`, ctrl.read);
+app.post(`/api/addBook`, ctrl.create);
+app.delete(`/api/`, ctrl.delete);
+app.put(`/api/`, ctrl.update);
+
+
 
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
